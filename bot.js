99318d8c1b6f11246b16c66f4f98d0473bf737df
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-03-24 14:56:02 +0100

    begin of rewrite

---
+++ bot.js
@@ -0,0 +1,43 @@
+//modules
+const moment = require('moment');
+const randomcolor = require('randomcolor');
+const Discord = require("discord.js");
+
+//bot client
+const bot = new Discord.Client();
+
+//json(s)
+const config = require("./config.json");
+
+const commands = new Map();
+fs.readdir(`./cmd/`, (err, files) => {
+  if(err) console.error(err);
+  console.log(`Loading a total of ${files.length} commands.`);
+  files.map(f=> {
+    let props = require(`./cmd/${f}`);
+    console.log(`Loading Command: ${props.help.name}. :ok_hand:`);
+    commands.set(props.help.name, props);
+  });
+});
+
+bot.on('ready',() => {
+	console.log(`---------------------------------------------`)
+	console.log(`Connected! ${config.emojis.success}`);
+	console.log(`Logged in as ${bot.user.username}`);
+	console.log(`token = ${config.token}`);
+	console.log(`game = ${config.setgame}`);
+	console.log(`prefix = ${config.client.prefix}`);
+	console.log(`console emojis = ${config.emojis}`)
+	console.log(`---------------------------------------------`)
+	bot.user.setGame(config.setgame);
+});
+
+bot.on('message', msg => {
+  if(!msg.content.startsWith(config.prefix)) return;
+  var command = msg.content.split(" ")[0].slice(config.prefix.length);
+  var params = msg.content.split(" ").slice(1);
+  if(commands.has(command)) {
+    var cmd = commands.get(command);
+    cmd.run(bot, msg, params);
+  }
+});
