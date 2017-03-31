9444462628cba195ce33d10aab73ccc1bc67159c
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-03-31 13:35:58 +0200

    Create eval.js

---
+++ cmd/eval.js
@@ -0,0 +1,26 @@
+const config = require('../../config.json');
+
+module.exports = function (bot, message, args) {
+if (message.content.startsWith('[]' + 'eval')) {
+    if !(config.creator.Jimmy.includes(message.author.id)) return;
+    try {
+      var code = args.join(" ");
+      var evaled = eval(code);
+
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+
+      message.channel.sendCode("xl", clean(evaled));
+      message.delete();
+    } catch (err) {
+      message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+  }
+};
+
+function clean(text) {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
