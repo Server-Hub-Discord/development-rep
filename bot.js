0c2c3a1fbbcd0fec1c1d3cee6fd5565424893ded
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-04-04 09:01:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,43 +1,79 @@
-//modules
-const moment = require('moment');
-const randomcolor = require('randomcolor');
 const Discord = require("discord.js");
-const fs = require('fs');
-
-//bot client
-const bot = new Discord.Client();
-
-//json(s)
+const bot = new Discord.Client({ fetchAllMembers: true });
 const config = require("./config.json");
+const fs = require("fs");
+const moment = require("moment");
 
-const commands = new Map();
-fs.readdir(`./cmd/`, (err, files) => {
-  if(err) console.error(err);
-  console.log(`Loading a total of ${files.length} commands.`);
-  files.map(f=> {
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+fs.readdir("./cmd/", (err, files) => {
+  if (err) console.error(err);
+  log(`Loading a total of ${files.length} commands.`);
+  files.forEach(f => {
     let props = require(`./cmd/${f}`);
-    commands.set(props);
+    log(`Loading Command: ${props.help.name}. :ok_hand:`);
+    bot.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      bot.aliases.set(alias, props.help.name);
+    });
   });
 });
 
-bot.on('ready',() => {
-	console.log(`---------------------------------------------`)
-	console.log(`Connected! ${config.emojis.success}`);
-	console.log(`Logged in as ${bot.user.username}`);
-	console.log(`token = ${config.token}`);
-	console.log(`game = ${config.setgame}`);
-	console.log(`prefix = ${config.client.prefix}`);
-	console.log(`console emojis = ${config.emojis}`)
-	console.log(`---------------------------------------------`)
-	bot.user.setGame(config.setgame);
-});
-
-bot.on('message', msg => {
-  if(!msg.content.startsWith(config.prefix)) return;
-  var command = msg.content.split(" ")[0].slice(config.prefix.length);
-  var params = msg.content.split(" ").slice(1);
-  if(commands.has(command)) {
-    var cmd = commands.get(command);
-    cmd(bot, msg, params, config);
+bot.on("message", msg => {
+  if (!msg.content.startsWith(config.prefix)) return;
+  let command = msg.content.split(" ")[0].slice(config.prefix.length);
+  let params = msg.content.split(" ").slice(1);
+  let perms = bot.elevation(msg);
+  let cmd;
+  if (bot.commands.has(command)) {
+    cmd = bot.commands.get(command);
+  } else if (bot.aliases.has(command)) {
+    cmd = bot.commands.get(bot.aliases.get(command));
+  }
+  if (cmd) {
+    if (perms < cmd.conf.permLevel) return;
+    cmd.run(bot, msg, params, perms, config);
   }
 });
+
+bot.on("ready", () => {
+  log(`GuideBot: Ready to serve ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} servers.`);
+});
+
+bot.on("error", console.error);
+bot.on("warn", console.warn);
+
+bot.login(config.botToken);
+
+bot.reload = function(command) {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./cmd/${command}`)];
+      let cmd = require(`./cmd/${command}`);
+      bot.commands.delete(command);
+      bot.aliases.forEach((cmd, alias) => {
+        if (cmd === command) bot.aliases.delete(alias);
+      });
+
+      bot.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        bot.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+bot.elevation = function(msg) {
+  /* This function should resolve to an ELEVATION level which
+     is then sent to the command handler for verification*/
+  let permlvl = 0;
+  let mod_role = msg.guild.roles.find("name", "Mods");
+  if(mod_role && msg.member.roles.has(mod_role.id)) permlvl = 2;
+  let admin_role = msg.guild.roles.find("name", "Devs");
+  if(admin_role && msg.member.roles.has(admin_role.id)) permlvl = 3;
+  if(msg.author.id === config.ownerid) permlvl = 4;
+  return permlvl;
+};
