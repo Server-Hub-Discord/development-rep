ae21fea9b4bf191ddeb6ce2783e66a5434a9e8d7
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-04-04 08:56:51 +0200

    Update eval.js

--- cmd/eval.js
+++ cmd/eval.js
@@ -1,24 +1,34 @@
-module.exports = function (bot, message, args, config) {
-if (message.content.startsWith('[]' + 'eval')) {
-    if (!config.creator.Jimmy.includes(message.author.id)) return;
-    try {
-      var code = args.join(" ");
-      var evaled = eval(code);
-
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
-
-      message.channel.sendCode("xl", clean(evaled));
-      message.delete();
-    } catch (err) {
-      message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    }
+exports.run = (bot, msg, params = []) => {
+  var code = params.join(" ");
+  try {
+    var evaled = eval(code);
+    if (typeof evaled !== 'string')
+      evaled = require('util').inspect(evaled);
+    msg.channel.sendMessage("```xl\n" + clean(evaled) + "\n```");
+  } catch (err) {
+    msg.channel.sendMessage("`ERROR` ```xl\n" +
+      clean(err) +
+      "\n```");
   }
 };
 
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['ev'],
+  permLevel: 4
+};
+
+exports.help = {
+  name: "eval",
+  description: "Evaluates arbitrary Javascript. Not for the faint of heart.\nExpression may contain multiple lines. Oh and **you** can't use it.",
+  usage: "eval <expression>"
+};
+
 function clean(text) {
-  if (typeof(text) === "string")
+  if (typeof(text) === "string") {
     return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-  else
-      return text;
+  } else {
+    return text;
+  }
 }
