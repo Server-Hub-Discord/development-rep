c4ada06eb3054bb4ba9e9828fb08a67a7bdb0c6c
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-04-05 08:59:21 +0200

    Update ping.js

--- cmd/ping.js
+++ cmd/ping.js
@@ -3,3 +3,16 @@ exports.run = (bot, message) => {
         .then(msg => msg.edit(`Pong! \`${msg.createdTimestamp - message.createdTimestamp}ms\``))
         .catch(console.error);
 };
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name : "ping",
+  description: "checks bot's reply speed",
+  usage: "ping"
+};
