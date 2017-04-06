8f89b901b9ee12357fc090d8be87a2566ffa2af8
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-04-06 08:38:27 +0200

    Create setstatus.js

---
+++ cmd/setstatus.js
@@ -0,0 +1,16 @@
+exports.run = (bot, msg, params) => {
+  bot.user.setStatus(params);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [setstat],
+  permLevel: 4
+};
+
+exports.help = {
+  name : "setstatus",
+  description: "changes the bots status",
+  usage: "setstatus [online || invisible || idle || dnd]"
+};
