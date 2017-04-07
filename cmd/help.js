c8341a8bd26387ec7a3fff3e29a0e7b58471b7c6
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-04-07 13:34:12 +0200

    Update help.js

--- cmd/help.js
+++ cmd/help.js
@@ -13,7 +13,7 @@ exports.run = (bot, message, params) => {
       let modRole = message.guild.roles.find("name", "Staff");
       let adminRole = message.guild.roles.find("name", "Owner");
       var cmds = ``;
-      cmds += `**Warning this is the dev rep so not all commands are usable or fully functional** \n\n **My Normal Commands are:** \n ${config.client.prefix}membercount \n ${config.client.prefix}serverinfo \n ${config.client.prefix}botservers \n ${config.client.prefix}date \n ${config.client.prefix}sourcecode \n ${config.client.prefix}avatar \n ${config.client.prefix}ping \n ${config.client.prefix}creator \n ${config.client.prefix}help \n ${config.client.prefix}stats \n ${config.client.prefix}myuserinfo`;
+      cmds += `**Warning this is the dev rep so not all commands are usable or fully functional** \n\n **My Normal Commands are:** \n ${config.client.prefix}membercount \n ${config.client.prefix}serverinfo \n ${config.client.prefix}botservers \n ${config.client.prefix}date \n ${config.client.prefix}sourcecode \n ${config.client.prefix}avatar \n ${config.client.prefix}ping \n ${config.client.prefix}creator \n ${config.client.prefix}help \n ${config.client.prefix}stats \n ${config.client.prefix}userinfo`;
       if (message.member.roles.has(modRole.id) || config.creator.Jimmy.includes(message.author.id)) {
           cmds += `\n\n **My Staff commands are** \n ${config.client.prefix}embed [what you want to embed] \n ${config.client.prefix}addrole {user} [role] \n ${config.client.prefix}delrole {user} [role] \n ${config.client.prefix}announce [what you want to announce in #announcements] \n ${config.client.prefix}say [what you want to say] \n ${config.client.prefix}kick {user} \n \n more details on how to use these commands coming soon`;
       }
