470fa5116f47fb13a7cf82e688a67696cf1e237b
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-04-07 13:20:38 +0200

    Create sourcecode.js

---
+++ cmd/sourcecode.js
@@ -0,0 +1,19 @@
+exports.run = (bot, message) => {
+    let noto = 'Main Repository https://github.com/Server-Hub-Discord/Staff-Mod-Bot \n  Dev repository (the bot is running on dev right now https://github.com/Server-Hub-Discord/development-rep/) ';
+		message.channel.sendMessage("", {embed: {
+		title: noto
+	}}).catch(console.error);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name : "sourcecode",
+  description: "sends a link to the repositories",
+  usage: "sourcecode"
+};
