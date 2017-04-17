b0c99cc29a38f0a78cd58aeaf28610d0b088dc34
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-04-17 09:24:34 +0200

    Update help.js

--- cmd/help.js
+++ cmd/help.js
@@ -13,12 +13,41 @@ exports.run = (bot, message, params) => {
       let modRole = message.guild.roles.find("name", "Staff");
       let adminRole = message.guild.roles.find("name", "Owner");
       var cmds = ``;
-      cmds += `**Warning this is the dev rep so not all commands are usable or fully functional** \n\n **My Normal Commands are:** \n ${config.client.prefix}membercount \n ${config.client.prefix}serverinfo \n ${config.client.prefix}botservers \n ${config.client.prefix}date \n ${config.client.prefix}sourcecode \n ${config.client.prefix}avatar \n ${config.client.prefix}ping \n ${config.client.prefix}creator \n ${config.client.prefix}help \n ${config.client.prefix}stats \n ${config.client.prefix}userinfo`;
+      cmds += `**Warning this is the dev rep so not all commands are fully functional** \n\n **My Normal Commands are:** \n
+                ${config.client.prefix}membercount \n
+                ${config.client.prefix}serverinfo \n 
+                ${config.client.prefix}botservers \n 
+                ${config.client.prefix}date \n 
+                ${config.client.prefix}sourcecode \n 
+                ${config.client.prefix}avatar \n 
+                ${config.client.prefix}ping \n 
+                ${config.client.prefix}help \n 
+                ${config.client.prefix}stats \n 
+                ${config.client.prefix}userinfo \n 
+                ${config.client.prefix}uptime \n
+                ${config.client.prefix}wiki \n
+                ${config.client.prefix}google \n
+                ${config.client.prefix}userinfo \n
+                ${config.client.prefix}githublinkmaker
+                `;
       if (message.member.roles.has(modRole.id) || config.creator.Jimmy.includes(message.author.id)) {
-          cmds += `\n\n **My Staff commands are** \n ${config.client.prefix}embed [what you want to embed] \n ${config.client.prefix}addrole {user} [role] \n ${config.client.prefix}delrole {user} [role] \n ${config.client.prefix}announce [what you want to announce in #announcements] \n ${config.client.prefix}say [what you want to say] \n ${config.client.prefix}kick {user} \n \n more details on how to use these commands coming soon`;
+          cmds += `\n\n **My Staff commands are** \n 
+                ${config.client.prefix}embed [what you want to embed] \n 
+                ${config.client.prefix}addrole {user} [role] \n 
+                ${config.client.prefix}delrole {user} [role] \n 
+                ${config.client.prefix}announce [what you want to announce in #announcements] \n 
+                ${config.client.prefix}say [what you want the bot to say] \n 
+                ${config.client.prefix}kick {user} \n 
+
+                \n more details on how to use these commands coming soon`;
       }
       if (message.member.roles.has(adminRole.id) || config.creator.Jimmy.includes(message.author.id)) {
-          cmds += `\n\n **My Owner/Creator Commands are:** \n ${config.client.prefix}setbotavatarurl (only Jimmy) \n ${config.client.prefix}setstatus (only Jimmy) \n ${config.client.prefix}shutdown \n ${config.client.prefix}restart`;
+          cmds += `\n\n **My Owner/Creator Commands are:** \n 
+                ${config.client.prefix}setbotavatarurl (only Jimmy) \n 
+                ${config.client.prefix}setstatus (only Jimmy) \n
+                ${config.client.prefix}setgame (only Jimmy) \n 
+                ${config.client.prefix}shutdown \n 
+                ${config.client.prefix}restart`;
       }
       message.author.sendMessage(" ", {
           embed: {
@@ -41,7 +70,7 @@ exports.run = (bot, message, params) => {
 
 exports.conf = {
   enabled: true,
-  guildOnly: false,
+  guildOnly: true,
   aliases: [],
   permLevel: 0
 };
