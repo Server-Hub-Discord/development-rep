f9fb749e2c9c51f0e31110bd8bd66058c3dd7e2c
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-04-18 11:31:23 +0200

    Create ban.js

---
+++ cmd/ban.js
@@ -0,0 +1,25 @@
+exports.run = (bot, message) => {
+    let args = message.content.split(" ").slice(1);
+    let userToKick = message.mentions.users.first() ? message.mentions.users.first() : args;
+    if (!userToKick) {
+        message.channel.sendMessage(` name : "ban",
+                                      description: "bans the selected user",
+                                      usage: "ban {user mention or user id}"`);
+        return;
+    }
+    message.guild.member(userToKick).kick().catch(console.error);
+    message.channel.sendMessage(`kicked.`).catch(console.error);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 2
+};
+
+exports.help = {
+  name : "ban",
+  description: "bans the selected user",
+  usage: "ban {user mention or user id}"
+};
