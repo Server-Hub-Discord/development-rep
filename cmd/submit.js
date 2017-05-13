d74a6ac2c0493cd2f88bb3ee3d7a383d07059546
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-05-13 17:32:36 +0200

    Update submit.js

--- cmd/submit.js
+++ cmd/submit.js
@@ -1,20 +1,10 @@
 exports.run = (bot, message, params) => {
-	let CHannel = message.mentions.channels.first() || null;
-	if (!(CHannel === null)) {
-		let messageContent = message.content.split(" ").slice(2).join(" ");
-  			CHannel.sendMessage(messageContent).catch(console.error);
-			message.channel.sendMessage("Done :white_check_mark:");
-			return;
-
-	} else if (!(CHannel) && (params)){
-	message.channel.sendMessage(params);
+	let CHannel = message.mentions.channels.first();
+	if (!CHannel) {return;}
+	let messageContent = message.content.split(" ").slice(2).join(" ");
+  	CHannel.sendMessage(messageContent).catch(console.error);
+	message.channel.sendMessage("Done :white_check_mark:");
 	return;
-} else if (!(CHannel) && !(params)){
-	message.channel.sendMessage(`name : "submit", \n
-  description: "makes the bot send a message in the channel specified", \n
-  usage: "submit [the submitted form]"`);
-}
-
 
 };
 
